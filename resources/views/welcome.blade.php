4a56682ad955ba0d5c14c4a70724cb81cd972d7f
Lilith-info <L17181087@slp.tecnm.mx>
2021-11-20 21:25:49 -0600

    Mi nombre

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,6 +64,8 @@
         </style>
     </head>
     <body>
+        <h1 style="color:#FFF;">Jetzyy Loredo</h1><br/>
+        
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
